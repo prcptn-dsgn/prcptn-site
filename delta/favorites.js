be4b376b0015bace5af0be4f4384525ecac9ee96
FakeDazon <44048306+FakeDazon@users.noreply.github.com>
2020-01-17 21:04:20 -0800

    changed size of palettes and added dynamic contrast

--- delta/favorites.js
+++ delta/favorites.js
@@ -61,7 +61,7 @@ updateMode();
 function createDiv(h1, h2, h3, h4, h5, name) {
 
     document.getElementById("main").innerHTML =
-        '<div class="group"><i class="fas fa-times delete" onclick="deleteCookie(' + "'" + name + "'" + ')"></i><a href="index.html#' + h1 + h2 + h3 + h4 + h5 + '"><div class="color" style="background: ' + "#" + h1 +
+        '<div class="group"><i class="fas fa-times delete" onclick="deleteCookie(' + "'" + name + "'" + ')" style="color:' + getContrast(h5) +'"></i><a href="index.html#' + h1 + h2 + h3 + h4 + h5 + '"><div class="color" style="background: ' + "#" + h1 +
         ';"></div><div class="color" style="background: ' + "#" + h2 +
         ';"></div><div class="color" style="background: ' + "#" + h3 +
         ';"></div><div class="color" style="background: ' + "#" + h4 +
@@ -137,6 +137,32 @@ function updateMode(){
 }
 function getScrollX(){
     var scroll = window.scrollY;
-    var goalVH = scroll / window.innerHeight * 100 + 10;
+    var goalVH = scroll / window.innerHeight * 100 + 40;
     document.getElementById("settings").style.top = goalVH + "vh";
 }
+var getContrast = function (hexcolor){
+
+	// If a leading # is provided, remove it
+	if (hexcolor.slice(0, 1) === '#') {
+		hexcolor = hexcolor.slice(1);
+	}
+
+	// If a three-character hexcode, make six-character
+	if (hexcolor.length === 3) {
+		hexcolor = hexcolor.split('').map(function (hex) {
+			return hex + hex;
+		}).join('');
+	}
+
+	// Convert to RGB value
+	var r = parseInt(hexcolor.substr(0,2),16);
+	var g = parseInt(hexcolor.substr(2,2),16);
+	var b = parseInt(hexcolor.substr(4,2),16);
+
+	// Get YIQ ratio
+	var yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
+
+	// Check contrast
+	return (yiq >= 128) ? 'black' : 'white';
+
+};
